b3202366458f4a85440bc4b2d4977449a61ecff0
Александр Ермилов <aleksandrermilov@MacBook-Air-Aleksandra.local>
2022-11-16 21:54:12 +0300

    add open search form func

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,53 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-  const { amount } = useSelector((store) => store.cart)
+  const { amount } = useSelector((store) => store.cart);
+  const openSearchForm = () => {
+    const form = document.querySelector('[data-id="search-form"]');
+    const input = document.querySelector(".form-control");
+    form.classList.toggle("invisible");
+    input.focus();
+  };
   return (
     <header className="container">
       <div className="row">
         <div className="col">
           <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <a className="navbar-brand" href="/">
-            <img src="./img/header-logo.png" alt="Bosa Noga"/>
+              <img src="./img/header-logo.png" alt="Bosa Noga" />
             </a>
             <div className="collapse navbar-collapse" id="navbarMain">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/" end>Главная</NavLink>
+                  <NavLink className="nav-link" to="/" end>
+                    Главная
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/catalog">Каталог</NavLink>
+                  <NavLink className="nav-link" to="/catalog">
+                    Каталог
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/about">О магазине</NavLink>
+                  <NavLink className="nav-link" to="/about">
+                    О магазине
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/contacts">Контакты</NavLink>
+                  <NavLink className="nav-link" to="/contacts">
+                    Контакты
+                  </NavLink>
                 </li>
               </ul>
               <div>
                 <div className="header-controls-pics">
-                  <div data-id="search-expander" className="header-controls-pic header-controls-search"></div>
+                  <div
+                    onClick={openSearchForm}
+                    data-id="search-expander"
+                    className="header-controls-pic header-controls-search"
+                  ></div>
                   {/* <!-- Do programmatic navigation on click to /cart.html --> */}
                   <div className="header-controls-pic header-controls-cart">
                     <div className="header-controls-cart-full">{amount}</div>
@@ -38,8 +56,11 @@ const Header = () => {
                     </div>
                   </div>
                 </div>
-                <form data-id="search-form" className="header-controls-search-form form-inline invisible">
-                  <input className="form-control" placeholder="Поиск"/>
+                <form
+                  data-id="search-form"
+                  className="header-controls-search-form form-inline invisible"
+                >
+                  <input className="form-control" placeholder="Поиск" />
                 </form>
               </div>
             </div>
